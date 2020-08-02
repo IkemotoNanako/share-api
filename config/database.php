f292fdc1bacd8521140093dd51c53baa01935da1
IkemotoNanako <chibencaizi@gmail.com>
2020-08-02 13:29:25 +0900

    pas

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'share-api'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'try75sql,'),
+            'password' => env('DB_PASSWORD', 'try75sql'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
